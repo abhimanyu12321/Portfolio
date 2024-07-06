5a14b8f70cf6b43ab28ab890a71ccf6bb3b521ef
Goru <abhiabhi9087@gmail.com>
2024-07-06 17:01:30 +0530

    mobile laayout fix

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212] gap-24">
       <Navbar />
-      <div className="container mt-24 mx-auto px-12 py-4">
+      <div className="container mt-24 mx-auto px-4 lg:px-12 py-4">
         <div id="home">
           <HeroSection />
         </div>
