d7e31978588d5bc5852c52042f84f7e2ee1cc688
Abhimanyu <abhimanyumajoka9087@gmail.com>
2024-11-17 17:10:53 +0530

    updated projects data

--- src/app/constants/Project.js
+++ src/app/constants/Project.js
@@ -1,34 +1,10 @@
 export const projectsData = [
     {
         id: 1,
-        title: "Amazon Clone Webapp",
-        description:
-            "Ecommerce webapp for shopping build with MERN Stack , MaterialUI. Have features which allows user to reset password via email, Authentication , can comment and rate products , and have Admin Panel , dashboard feature. ",
-        image: "/images/projects/1.png",
-        tag: ["All", "Web"],
-        gitUrl: "https://github.com/abhimanyu12321/Shopping-app-frontend",
-        previewUrl:
-            "https://amazona-clone-rho.vercel.app/",
-        isPublic: true,
-    },
-    {
-        id: 2,
-        title: "Freshbuyzar Ecommerce Webapp",
-        description:
-            "Ecommerce webapp for buying fruits and vegetables build with React , NextJS , TailwindCSS. Have features which allows user to add products to cart and order, Authentication using Mobile Otp and have online payment feature.",
-        image: "/images/projects/5.png",
-        tag: ["All", "Web"],
-        gitUrl: "https://github.com/iamkaushal/fb-frontend",
-        previewUrl:
-            "https://freshbuyzar.com/",
-        isPublic: false,
-    },
-    {
-        id: 3,
         title: "Sistah Chatta",
         description:
             "A Product reviewing webapp build with React , NextJS , TailwindCSS , and shadcn. Users can authenticate and view all products and give rating and review on each product. ",
-        image: "/images/projects/6.png",
+        image: "/images/projects/sistah.png",
         tag: ["All", "Web"],
         gitUrl: "https://github.com/iamkaushal/sistahchatta-frontend",
         previewUrl:
@@ -36,15 +12,40 @@ export const projectsData = [
         isPublic: false,
     },
     {
+        id: 2,
+        title: "Spend Craft UI",
+        description:
+            "Created a 7 page UI for a webapp build with React , Next,js ,TailwindCSS and shadcn. ",
+        image: "/images/projects/spend-craft.png",
+        tag: ["All", "Web"],
+        gitUrl: "https://github.com/abhimanyu12321/spend-craft-ui",
+        previewUrl: "https://spend-craft-ui.vercel.app/",
+        isPublic: false,
+    },
+
+    {
+        id: 3,
+        title: "Freshbuyzar Ecommerce Webapp",
+        description:
+            "Ecommerce webapp for buying fruits and vegetables build with React , NextJS , TailwindCSS. Have features which allows user to add products to cart and order, Authentication using Mobile Otp and have online payment feature.",
+        image: "/images/projects/freshbuyzar.png",
+        tag: ["All", "Web"],
+        gitUrl: "https://github.com/iamkaushal/fb-frontend",
+        previewUrl:
+            "https://freshbuyzar.com/",
+        isPublic: false,
+    },
+
+    {
         id: 4,
         title: "PauseMy.Cloud",
         description:
             "A Cloud service website build with React , NextJS , TailwindCSS , and shadcn.",
-        image: "/images/projects/7.png",
+        image: "/images/projects/pause-my-cloud.png",
         tag: ["All", "Web"],
         gitUrl: "https://github.com/iamkaushal/pausemycloud",
         previewUrl:
-            "https://pausemycloud.vercel.app/",
+            "https://pause-my-cloud-ui.vercel.app/",
         isPublic: false,
     },
     {
@@ -52,21 +53,23 @@ export const projectsData = [
         title: "AI CERTS Landing page",
         description:
             "A landing page for AI CERTS which is a certification platform  build with React , Next,js ,TailwindCSS. ",
-        image: "/images/projects/4.png",
+        image: "/images/projects/cert.png",
         tag: ["All", "Web"],
         gitUrl: "https://github.com/abhimanyu12321/AI-clerts-assignment",
         previewUrl: "https://ai-clerts-assignment.vercel.app/",
         isPublic: true,
     },
+
     {
         id: 6,
-        title: "Multi Language quiz",
+        title: "Amazon Clone Webapp",
         description:
-            "A quiz site where users choose one language , then go through some words and their translated words and then go through a quiz. The result with the user's answer and correct answer is shown at last, build with React , MaterialUI , TypeScript , Redux. Have text-to-voice feature. ",
-        image: "/images/projects/2.png",
+            "Ecommerce webapp for shopping build with MERN Stack , MaterialUI. Have features which allows user to reset password via email, Authentication , can comment and rate products , and have Admin Panel , dashboard feature. ",
+        image: "/images/projects/amazon.png",
         tag: ["All", "Web"],
-        gitUrl: "https://github.com/abhimanyu12321/Multilanguage-Quiz",
-        previewUrl: "https://multilanguage-quiz.vercel.app/",
+        gitUrl: "https://github.com/abhimanyu12321/Shopping-app-frontend",
+        previewUrl:
+            "https://amazona-clone-rho.vercel.app/",
         isPublic: true,
     },
 
