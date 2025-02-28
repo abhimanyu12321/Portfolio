7516ab950e167c632ab69cfe423e41b2e0aeeed6
Abhimanyu <abhimanyumajoka9087@gmail.com>
2025-02-28 13:19:53 +0530

    added particles

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,10 +6,11 @@ import ContactSection from "./components/ContactSection";
 import Footer from "./components/Footer";
 import SkillSection from "./components/SkillSection";
 import ExperienceSection from "./components/ExperienceSection";
+import { Particles } from "@/components/magicui/particles";
 // import AchievementsSection from "./components/AchievementsSection";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-black gap-2 md:gap-12">
+    <main className="flex min-h-screen flex-col bg-black gap-2 md:gap-12 relative">
       <Navbar />
       <div className="container mt-14 mx-auto px-4 lg:px-12 py-4">
         <div id="home">
@@ -33,6 +34,13 @@ export default function Home() {
           <ContactSection />
         </div>
       </div>
+      <Particles
+        className="absolute inset-0 z-0"
+        quantity={200}
+        ease={80}
+        color={"#ffffff"}
+        refresh
+      />
       <Footer />
     </main>
   );
